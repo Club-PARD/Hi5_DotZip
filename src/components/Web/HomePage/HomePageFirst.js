f19b7dd2560adbfeee003048cf3a9970ab3c9df1
Kim-Hyun-Seung <120882082+Kim-Hyun-Seung@users.noreply.github.com>
2023-06-24 18:07:47 +0900

    .

--- src/components/Web/HomePage/HomePageFirst.js
+++ src/components/Web/HomePage/HomePageFirst.js
@@ -1,18 +1,16 @@
-import styled from 'styled-components';
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { collection, query, where, getDocs } from 'firebase/firestore';
 import { dbService } from '../../../fbase';
+import { collection, onSnapshot } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
+import styled from 'styled-components';
 
-// Initialize Firebase Authentication
 const authService = getAuth();
 
 const Div = styled.div`
   margin-top: 70px;
   height: 1000px;
 `;
-
 
 const HeaderP = styled.p`
   width: 260px;
@@ -24,20 +22,6 @@ const HeaderP = styled.p`
   line-height: 19px;
   color: #818181;
 `;
-const Header3 = styled.p`
-width: 110px;
-height: 24px;
-top: 270px;
-left: 27px;
-font-family: Pretendard;
-font-size: 20px;
-font-weight: 700;
-line-height: 24px;
-letter-spacing: 0em;
-text-align: left;
-color: #EFEFEF;
-
-`;
 
 const HeaderDiv = styled.header`
   width: 120px;
@@ -47,8 +31,20 @@ const HeaderDiv = styled.header`
   font-weight: 700;
   font-size: 16px;
   line-height: 19px;
-  color: #EFEFEF;
+  color: #efefef;
   left: calc(50% - 311px/2);
+`;
+
+const Header3 = styled.p`
+  width: 110px;
+  height: 24px;
+  font-family: Pretendard;
+  font-size: 20px;
+  font-weight: 700;
+  line-height: 24px;
+  letter-spacing: 0em;
+  text-align: left;
+  color: #efefef;
 `;
 
 const Header2 = styled.div`
@@ -56,8 +52,9 @@ const Header2 = styled.div`
   flex-direction: column;
   align-items: flex-start;
   justify-content: center;
-  margin-top: 20px; /* Adjust this value to move it higher */
+  margin-top: 20px;
 `;
+
 const Survey = styled.div`
   display: flex;
   flex-direction: column;
@@ -70,50 +67,28 @@ const Survey = styled.div`
   overflow-x: hidden;
 `;
 
-
-
-const HomeFirst = (kakaoid, questionid, answerid) => {
+const HomePageFirst = () => {
   const navigate = useNavigate();
   const [currentUser, setCurrentUser] = useState(null);
+  const [userId, setUserId] = useState(null);
 
-  useEffect(() => {
-    const unsubscribe = authService.onAuthStateChanged((user) => {
-      if (user) {
-        setCurrentUser(user);
-      } else {
-        setCurrentUser(null);
-        console.log('User not logged in');
-      }
-    });
+// ...
 
-    return () => {
-      unsubscribe();
-    };
-  }, []);
+useEffect(() => {
+  const unsubscribe = onSnapshot(collection(dbService, 'kakaoId'), (snapshot) => {
+    const userIds = snapshot.docs.map((doc) => doc.data().userId);
+    const firstUserId = userIds[0];
+    console.log("현재 사용자의 userId:", firstUserId);
+    // 원하는 로직을 추가하세요.
+  });
 
-  useEffect(() => {
-    if (currentUser) {
-      const userId = currentUser.uid;
-      console.log('userId:', userId); // For debugging purposes
+  return () => {
+    unsubscribe(); // 컴포넌트가 언마운트될 때 데이터 변경 구독을 해제합니다.
+  };
+}, []);
 
-      const fetchData = async () => {
-        const q = query(collection(dbService, 'kakaoId'), where('userId', '==', userId));
-        const snapshot = await getDocs(q);
+// ...
 
-        if (snapshot.empty) {
-          console.log('User ID does not exist in "kakaoId" collection:', userId);
-        } else {
-          snapshot.forEach((doc) => {
-            const data = doc.data();
-            console.log('User ID exists in "kakaoId" collection:', data.userId);
-            // Perform other tasks here
-          });
-        }
-      };
-
-      fetchData();
-    }
-  }, [currentUser]);
 
   const handleButtonClick = () => {
     navigate('../../../../SurveyFirst'); // Replace with the actual path you want to navigate to
@@ -124,18 +99,18 @@ const HomeFirst = (kakaoid, questionid, answerid) => {
   };
 
   return (
-  <Div>
-    <Survey>
-      <Header2>
-        <HeaderDiv>안녕하세요, OO님</HeaderDiv>
-        <HeaderP>궁금한 질문을 담은 링크를 공유해보세요.</HeaderP>
-      </Header2>
-      <button onClick={handleButtonClick}>나의 .Zip</button>
-      <button onClick={handleButton1Click}>새로운 .Zip 만들기 </button>
-      <Header3>진행중인 .Zip</Header3>
-    </Survey>
-  </Div>
-);
-    }
+    <Div>
+      <Survey>
+        <Header2>
+          <HeaderDiv>안녕하세요, OO님</HeaderDiv>
+          <HeaderP>궁금한 질문을 담은 링크를 공유해보세요.</HeaderP>
+        </Header2>
+        <button onClick={handleButtonClick}>나의 .Zip</button>
+        <button onClick={handleButton1Click}>새로운 .Zip 만들기</button>
+        <Header3>진행중인 .Zip</Header3>
+      </Survey>
+    </Div>
+  );
+};
 
-export default HomeFirst;
+export default HomePageFirst;
