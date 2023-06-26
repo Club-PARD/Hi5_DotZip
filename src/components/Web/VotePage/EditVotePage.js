2f41472243155619488d6a29b8f319426e635c97
LeeHannaa <tiger5391@naver.com>
2023-06-26 23:51:21 +0900

    EditVotePage

--- src/components/Web/VotePage/EditVotePage.js
+++ src/components/Web/VotePage/EditVotePage.js
@@ -1,8 +1,9 @@
 import styled from "styled-components";
-import React, { useState, useEffect } from 'react';
-import { collection, query, onSnapshot } from 'firebase/firestore';
+import React, { useState, useEffect, useContext } from 'react';
+import { collection, getDocs } from 'firebase/firestore';
 import { dbService } from '../../../fbase';
 import { useNavigate } from 'react-router-dom';
+import { UserNameContext } from '../../../UserNameContext';
 
 const Div = styled.div`
 
@@ -54,25 +55,51 @@ const ReturnVote = styled.button`
 `;
 
 function EditVotePage() {
+    const [userContext] = useContext(UserNameContext);
+    const QuestionId = '6LOxO0d0kqHvGEY2Sfbc'; //questionid 받아오기
+    const [questionzip, setQuestionZip] = useState();
+    const [targetQid, setTargetQidZip] = useState();
+    const [commentzip, setCommentZip] = useState();
     //답변 불러오기
     const [answerzips, setAnswerZip] = useState([]);
     useEffect(() => {
-        const fetchData = async () => {
-            const q = query(collection(dbService, "zip"));
-            const unsubscribe = onSnapshot(q, (snapshot) => {
-            const zipArr = snapshot.docs.map((doc) => ({
+        fetchDataQuestion();
+    });
+    const fetchDataQuestion = async () => {
+        const zipCollection = collection(dbService, "zip_Question");
+        getDocs(zipCollection)
+        .then((zipSnapshot) => {
+            const QuestionzipArr = zipSnapshot.docs.map((doc) => ({
                 id: doc.id,
                 ...doc.data(),
-            }));
-            setAnswerZip(zipArr);
-            });
-            return () => {
-            unsubscribe();
-            };
+        }));
+        const targetQ = QuestionzipArr.find((user) => user.id === QuestionId);
+        setTargetQidZip(targetQ.id);
+        setQuestionZip(targetQ.question); //질문 questionzip에 저장
+        setCommentZip(targetQ.comment); //comment commentzip에 저장
+        });
+        fetchDataAnswer();
+        return () => {
+            zipCollection(); // 감시 중지
         };
-        
-        fetchData();
-    }, []);
+    };
+    const fetchDataAnswer = async () => {
+        const answerZipCollection = collection(dbService, "zip_Answer");
+        getDocs(answerZipCollection)
+        .then((answerZipSnapshot) => {
+            const answerZipArr = answerZipSnapshot.docs.map((doc) => ({
+                id: doc.id,
+                ...doc.data(),
+        }));
+        const targetAnswers = answerZipArr.filter(
+            (answer) => answer.questionId === targetQid
+          );
+          setAnswerZip(targetAnswers);
+        });
+        return () => {
+          answerZipCollection(); // 감시 중지
+        };
+    };
 
     const navigate = useNavigate();
     const handlePickVote = () => {
@@ -82,19 +109,21 @@ function EditVotePage() {
         <Div>
             <h1>나의 .ZiP 만들기</h1>
             <h3>궁금한 질문을 담은 링크를 공유해보세요.</h3>
-            <h4>'userid받아서 이름' 님의 .ZiP</h4> {/* 유저 닉네임 받아서 넣을 부분 */}
+            <h4>{userContext}님의 .ZiP</h4>
             <Emoticon>이모지 구현 칸</Emoticon>
-            <Title>questionId받아서 질문 타이틀 뜨기</Title>  {/* questionId받아서 질문 타이틀 */}
-            <Coment>questionId받아서 기본 코멘트 뜨도록 구현</Coment> {/* questionId받아서 기본 코멘트 */}
+            <Title>{questionzip}</Title>
+            <Coment>{commentzip}</Coment>
             {answerzips.map((answerzip) => (
-                <div key={answerzip.id}>
-                    {answerzip.answer.map((answerItem) => (
-                        <VoteBox key={answerzip.id}>
-                            <span>{answerItem}</span>
-                        </VoteBox>
-                    ))}
+            <div key={answerzip.id}>
+                <VoteBox>
+                <div>
+                    <span>
+                    {answerzip.answer}
+                    </span>
                 </div>
-            ))}
+                </VoteBox>
+            </div>
+        ))}
             <AddWord>항목 추가하기</AddWord> {/* 현승오빠가 구현해놓은 부분으로 => 항목 추가하는 부분 */}
             <ReturnVote onClick={handlePickVote}>투표 등록하기</ReturnVote>
         </Div>
