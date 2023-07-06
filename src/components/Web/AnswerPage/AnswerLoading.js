c14e02d8c0e8acb0c7915567962d2ff731489353
Kim-Hyun-Seung <120882082+Kim-Hyun-Seung@users.noreply.github.com>
2023-07-06 11:24:48 +0900

    hi

--- src/components/Web/AnswerPage/AnswerLoading.js
+++ src/components/Web/AnswerPage/AnswerLoading.js
@@ -2,14 +2,22 @@ import React, { useEffect } from 'react';
 import Animation from './Animation';
 import styled from 'styled-components';
 import { useNavigate, useParams } from 'react-router-dom';
+import AnswerNavBar from "../../AnswerNavbar"
 
 const Div = styled.div`
-width: 300px;
+width: 327px;
 margin: 0px auto;
 display: flex;
 flex-direction: column;
 align-items: center;
 text-align: center;
+`;
+const Container = styled.div`
+display: flex;
+flex-direction: column;
+text-align: left;
+margin-top: 100px;
+width: 100%;
 `;
 const Header1=styled.div`
 color: var(--gray-90, #353535);
@@ -20,7 +28,7 @@ font-family: PretendardBold;
 font-style: normal;
 font-weight: 700;
 line-height: 28px;
-margin-top: 100px;
+
 `;
 
 
@@ -51,7 +59,12 @@ const AnswerLoading = () => {
       }, []);
   return (
     <Div>
-    <Header1>답변 가고 있는중...</Header1>
+      <AnswerNavBar/>
+      <Container>
+        <></>
+    <Header1>작성해주신 답변을</Header1>
+    <Header1>질문 폴더로 전달하고 있어요! </Header1>
+    </Container>
     {/* <Header2></Header2> */}
         <Animation/>
     </Div>
