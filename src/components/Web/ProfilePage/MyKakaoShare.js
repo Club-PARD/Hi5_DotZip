9903c44cf5ac7798ad1f5bc7339d3e9d352e4ab3
LeeHannaa <tiger5391@naver.com>
2023-08-25 15:38:56 +0900

    update kakao share

--- src/components/Web/ProfilePage/MyKakaoShare.js
+++ src/components/Web/ProfilePage/MyKakaoShare.js
@@ -1,4 +1,3 @@
-//카카오톡으로 공유하는 기능 구현
 import React, { useEffect, useState } from 'react';
 import styled from "styled-components";
 import KakaoImoticon from '../../../img/Share.png';
@@ -10,9 +9,11 @@ const ShareButton = styled.img`
     margin-left: 24px;
     cursor: pointer;
 `;
+
 const MyKakaoShareButton = () => {
   const [userNickname] = useState(localStorage.getItem("userName"));
   const hashId = 3 * localStorage.getItem("kakaoId");
+
   useEffect(() => {
     createKakaoButton();
   }, []);
@@ -24,41 +25,42 @@ const MyKakaoShareButton = () => {
         if (!kakao.isInitialized()) {
           kakao.init('7eee69554b01a9bd34081035d074123e');
         }
+        
+        kakao.Link.createDefaultButton({
+          container: '#kakaotalk-sharing-btn',
+          objectType: 'feed',
+          content: {
+            title: `${userNickname}님이 프로필을 공유했어요!`,
+            description: `프로필.ZiP을 \n지금 바로 확인해보세요.`,
+            imageUrl: 'https://github.com/Club-PARD/Hi5_DotZip/blob/master/src/img/thumbnail.png?raw=true',
+            link: {
+              mobileWebUrl: `${window.location.origin}/MyProfileSharePage/${hashId}`,
+              webUrl: `${window.location.origin}/MyProfileSharePage/${hashId}`,
+            },
+          },
+          buttons: [
+            {
+              title: '프로필 구경하러 가기',
+              link: {
+                mobileWebUrl: `${window.location.origin}/MyProfileSharePage/${hashId}`,
+                webUrl: `${window.location.origin}/MyProfileSharePage/${hashId}`,
+              },
+            },
+          ],
+        });
       }
     } catch (e) {
       alert(e.name);
       alert(e.description);
     }
-
-    Kakao.Share.createDefaultButton({
-      container: '#kakaotalk-sharing-btn',
-      objectType: 'feed',
-      content: {
-        title: `${userNickname}님이 프로필을 공유했어요!`, // 사용자 id받아와서 넣어 줄 부분
-        description: `프로필.ZiP을 \n지금 바로 확인해보세요.`,
-        imageUrl: 'https://github.com/Club-PARD/Hi5_DotZip/blob/master/src/img/thumbnail.png?raw=true',
-        link: {
-          mobileWebUrl: `${window.location.origin}/MyProfileSharePage/${hashId}`,
-          webUrl: `${window.location.origin}/MyProfileSharePage/${hashId}`,
-        },
-      },
-      buttons: [
-        {
-          title: '프로필 구경하러 가기',
-          link: {
-            mobileWebUrl: `${window.location.origin}/MyProfileSharePage/${hashId}`,
-            webUrl: `${window.location.origin}/MyProfileSharePage/${hashId}`,
-          },
-        },
-      ],
-    });
   };
 
   return (
     <div>
-      <ShareButton id="kakaotalk-sharing-btn" onClick={createKakaoButton} src={KakaoImoticon} />
+      <ShareButton id="kakaotalk-sharing-btn" src={KakaoImoticon} />
     </div>
   );
 };
 
 export default MyKakaoShareButton;
+
